92593bd9813a1ea2ab031e09b9de2819ce2fe36e
jackhr <jc2o@mac.com>
2024-11-15 16:25:47 -0500

    1) Add head.php 2) Add foot.php 3) Add connection.php 4) Add basic styling 5) Add index.php

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+<?php require_once 'connection.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $company_name; ?></title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require_once __DIR__ . '/includes/head.php'; ?>
+
+<body>
+
+    <h1>Hello, World!</h1>
+
+</body>
+
+<?php require_once __DIR__ . '/includes/foot.php'; ?>
