3d8c49d978bb6f01535880ea48bb0e3731078154
jackhr <jc2o@mac.com>
2024-11-17 00:28:48 -0500

    Add name panels section

--- index.php
+++ index.php
@@ -47,6 +47,53 @@
         </div>
     </section>
 
+    <section id="panel-section">
+        <div class="inner">
+            <div id="name-panel-container">
+                <div class="name-panel imogen">
+                    <div class="left">
+                        <h2>Imogen</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ad reiciendis aspernatur dignissimos soluta perspiciatis tempore cumque, exercitationem quisquam provident delectus mollitia sed excepturi suscipit odio officia vel voluptatum rerum?</p>
+                        <a href="#">view more...</a>
+                    </div>
+                    <div class="right">
+                        <img src="/assets/images/panels/imogen.png" alt="">
+                    </div>
+                </div>
+                <div class="name-panel michael">
+                    <div class="left">
+                        <h2>Michael</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ad reiciendis aspernatur dignissimos soluta perspiciatis tempore cumque, exercitationem quisquam provident delectus mollitia sed excepturi suscipit odio officia vel voluptatum rerum?</p>
+                        <a href="#">view more...</a>
+                    </div>
+                    <div class="right">
+                        <img src="/assets/images/panels/michael.jpeg" alt="">
+                    </div>
+                </div>
+                <div class="name-panel lights">
+                    <div class="left">
+                        <h2>Lights</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ad reiciendis aspernatur dignissimos soluta perspiciatis tempore cumque, exercitationem quisquam provident delectus mollitia sed excepturi suscipit odio officia vel voluptatum rerum?</p>
+                        <a href="#">view more...</a>
+                    </div>
+                    <div class="right">
+                        <img src="/assets/images/panels/lights.png" alt="">
+                    </div>
+                </div>
+                <div class="name-panel commissions">
+                    <div class="left">
+                        <h2>Commissions</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ad reiciendis aspernatur dignissimos soluta perspiciatis tempore cumque, exercitationem quisquam provident delectus mollitia sed excepturi suscipit odio officia vel voluptatum rerum?</p>
+                        <a href="#">view more...</a>
+                    </div>
+                    <div class="right">
+                        <img src="/assets/images/panels/commissions.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
 </body>
 
 <?php require_once __DIR__ . '/includes/foot.php'; ?>
