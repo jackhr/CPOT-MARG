e27e7a8b14e2e9d684b90e2e7da9c49ec2a59066
jackhr <jc2o@mac.com>
2024-11-20 20:23:31 -0500

    Add favicon

--- includes/head.php
+++ includes/head.php
@@ -9,6 +9,13 @@
     <title><?php echo $company_name; ?></title>
     <link rel="stylesheet" href="/assets/css/style.css">
 
+    <!-- favicon begin -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/images/favicon/site.webmanifest">
+    <!-- favicon end -->
+
     <!-- BEGIN FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
