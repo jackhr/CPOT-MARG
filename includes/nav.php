e4deab175b0d78463871b918db486f363a873b09
jackhr <jc2o@mac.com>
2024-11-21 14:01:21 -0500

    Fix bug in nav that didn't hightlight cart icon when on cart page

--- includes/nav.php
+++ includes/nav.php
@@ -23,7 +23,7 @@ $current_url = $_SERVER['REQUEST_URI'];
             <a href="/contact" class="<?php echo ($current_url == '/contact/') ? 'current' : ''; ?>">Contact</a>
         </li>
         <li>
-            <a href="/cart" class="<?php echo ($current_url == '/contact/') ? 'current' : ''; ?>">
+            <a href="/cart" class="<?php echo ($current_url == '/cart/') ? 'current' : ''; ?>">
                 <svg title="Cart" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <circle cx="8" cy="21" r="1" />
