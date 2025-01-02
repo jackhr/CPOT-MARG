ceb68fb7a6d2699bf0489a0ab5fdb510113a70e9
jackhr <jc2o@mac.com>
2025-01-02 15:44:34 -0400

    Add inital backbone of contact page

---
+++ contact/index.php
@@ -0,0 +1,21 @@
+<?php require_once __DIR__ . '/../includes/head.php'; ?>
+
+<body>
+    <?php require_once __DIR__ . '/../includes/nav.php'; ?>
+
+    <section class="header">
+        <div class="inner">
+            <h1>Contact</h1>
+            <p>Connect with us to explore bespoke ceramic artistry—crafted with care, inspired by passion, and tailored to you.</p>
+        </div>
+    </section>
+
+    <section>
+        <div class="inner">
+            <h2>Contact Form Coming Soon!</h2>
+        </div>
+    </section>
+
+</body>
+
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
