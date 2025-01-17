1b9ab2ec4b14662414aa897cbc3365aad9cae4da
jackhr <jc2o@mac.com>
2025-01-17 15:42:23 -0500

    Update contact email functionality

--- api/contact/api.php
+++ api/contact/api.php
@@ -68,12 +68,25 @@ $email_subject = "Contact Form at Margrie Hunt";
 $from = "contact";
 
 try {
+    // Get email body
+    $template_path = __DIR__ . "/emails/contact/client.html";
+
+    if (!file_exists($template_path)) {
+        throw new Error("Email template file not found: $template_path");
+    }
+
+    $email_body = file_get_contents($template_path);
+
+    // Replace placeholders with actual data
+    foreach ($html_data as $key => $value) {
+        $email_body = str_replace("{{{$key}}}", htmlspecialchars($value, ENT_QUOTES, 'UTF-8'), $email_body);
+    }
+
     // Send email to the client
     $mail_res_client = handleSendEmail(
         $from,
         $data['email'],
-        "$from/client.html",
-        $data,
+        $email_body,
         $email_subject
     );
 
@@ -86,12 +99,25 @@ try {
         $admin_email_str = $email_string;
     }
 
+    // Get email body
+    $template_path = __DIR__ . "/emails/contact/admin.html";
+
+    if (!file_exists($template_path)) {
+        throw new Error("Email template file not found: $template_path");
+    }
+
+    $email_body = file_get_contents($template_path);
+
+    // Replace placeholders with actual data
+    foreach ($html_data as $key => $value) {
+        $email_body = str_replace("{{{$key}}}", htmlspecialchars($value, ENT_QUOTES, 'UTF-8'), $email_body);
+    }
+
     // Send email to admin
     $mail_res_client = handleSendEmail(
         $from,
         $admin_email_str,
-        "$from/admin.html",
-        $data,
+        $email_body,
         $email_subject,
         $data['email']
     );
