7afd1edd3d5876611ead93cf00df614aed858c95
jackhr <jc2o@mac.com>
2025-01-23 14:35:23 -0500

    Fix SQL syntax error

--- api/cutouts/api.php
+++ api/cutouts/api.php
@@ -27,8 +27,8 @@ if (isset($data['action'])) {
                 "SELECT cutouts.*, cutout_images.image_url
                     FROM cutouts
                     LEFT JOIN cutout_images ON cutouts.primary_image_id = cutout_images.image_id
-                    ORDER BY cutouts.name ASC
-                WHERE deleted_at IS NULL"
+                WHERE deleted_at IS NULL
+                ORDER BY cutouts.name ASC"
             );
             $stmt->execute();
             $res['data'] = $stmt->fetchAll(PDO::FETCH_ASSOC);
