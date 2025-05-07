223eb6bb79cdbccdacd507058f6a3d93d10ab956
jackhr <jc2o@mac.com>
2025-05-07 11:46:07 -0400

    UI Updates

--- includes/footer.php
+++ includes/footer.php
@@ -19,8 +19,24 @@ if (($current_url == '/sconces/') || ($current_url == '/sconces/shop.php')) {
                     <a href="tel:+1 (268) 460-5293">+1 (268) 460-5293</a>
                 </div>
                 <div class="contact-link">
+                    <span>Whatsapp:</span>
+                    <a target="_blank" href="https://wa.me/12687704061?text=Hello!%20I’m%20currently%20browsing%20your%20website%20and%20I’m%20interested%20in%20learning%20more%20about%20your%20products%2Fservices.%20Could%20you%20please%20provide%20additional%20information%20about...">+1 (268) 770-4061</a>
+                </div>
+                <div class="contact-link">
                     <span>Email:</span>
-                    <a href="mailto:info@<?php echo $domain; ?>">info@<?php echo $domain; ?></a>
+                    <a href="mailto:info@<?php echo $domain; ?>?subject=Inquiry%20about%20Products%2FServices&body=Hello!%20I’m%20currently%20browsing%20your%20website%20and%20I’m%20interested%20in%20learning%20more%20about%20your%20products%2Fservices.%20Could%20you%20please%20provide%20additional%20information%20about...">info@<?php echo $domain; ?></a>
+                </div>
+                <div class="social-icons">
+                    <div class="contact-link">
+                        <a target="_blank" href="https://wa.me/12687704061?text=Hello!%20I’m%20currently%20browsing%20your%20website%20and%20I’m%20interested%20in%20learning%20more%20about%20your%20products%2Fservices.%20Could%20you%20please%20provide%20additional%20information%20about...">
+                            <img src="/assets/icons/whatsapp.png" alt="Whatsapp Icon">
+                        </a>
+                    </div>
+                    <div class="contact-link">
+                        <a href="https://www.facebook.com/MargrieHunt" target="_blank">
+                            <img src="/assets/icons/facebook.png" alt="Facebook Icon">
+                        </a>
+                    </div>
                 </div>
             </div>
             <div class="footer-logo">
