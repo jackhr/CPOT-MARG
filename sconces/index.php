f2c418ab11336a18fb59aff8402c6da998c950d7
jackhr <jc2o@mac.com>
2025-09-14 20:43:27 -0400

    Update pdf viewer from Issuu to FlipHTML5

--- sconces/index.php
+++ sconces/index.php
@@ -63,8 +63,8 @@
     <section class="catalogue-section">
         <div class="inner">
             <h1>Product Catalogue</h1>
-            <div style="position:relative;padding-top:max(60%,326px);height:0;width:100%">
-                <iframe allow="clipboard-write" sandbox="allow-top-navigation allow-top-navigation-by-user-activation allow-downloads allow-scripts allow-same-origin allow-popups allow-modals allow-popups-to-escape-sandbox allow-forms" allowfullscreen="true" style="position:absolute;border:none;width:100%;height:100%;left:0;right:0;top:0;bottom:0;" src="https://e.issuu.com/embed.html?d=margrie_hunt_light_catalog_-_new&u=tropicalstudios"></iframe>
+            <div style="position:relative;padding-top:max(60%,324px);width:100%;height:0;">
+                <iframe style="position:absolute;border:none;width:100%;height:100%;left:0;top:0;" src="https://online.fliphtml5.com/qldpa/dabt/" seamless="seamless" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true"></iframe>
             </div>
         </div>
     </section>
